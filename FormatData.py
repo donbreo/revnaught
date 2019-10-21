c4874e0daa5eecc474bbd21ec398730d9f817598
donbreo <48909881+donbreo@users.noreply.github.com>
2019-10-21 23:21:53 +0530

    Update

--- FormatData.py
+++ FormatData.py
@@ -1,8 +1,10 @@
 import requests
 response = requests.get('https://www.quandl.com/api/v3/datasets/BSE/BOM533171.json?api_key=qz1RFMDVb5hEQ4qXxnUG')
-response.request.body
+
 print response #print http status
-print(response.request.body)
-print(response.request.url)
-print(response.content)
-print(response.text)
+print(response.request.body) #print body which is empty
+print(response.request.url) #prints the url duh
+print(response.content) #prints all the content that is visible in the browser
+print(response.text) #de-serializizes json data
+rdata = json.loads(response.text) #puts raw json data into variable
+data_serialized = json.dumps(data)
