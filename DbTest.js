c18a49b6c9f16d14e9c2839ed95da4ec8310b265
branabreo <github@brandondabreo.com>
2019-11-09 21:25:23 +0530

    Cleaning up files

--- DbTest.js
+++ DbTest.js
@@ -1,10 +1,8 @@
-var mongo = require('mongodb');
-
 //create a database
 
 var MongoClient = require('mongodb').MongoClient;
-var url = "mongodb://localhost:27017/bse";
-
+var url = "mongodb://localhost:27017/";
+/*
 MongoClient.connect(url, function(err, db) {
   if (err) throw err;
   console.log("Database created!");
@@ -22,3 +20,16 @@ MongoClient.connect(url, function(err, db) {
       db.close();
     });
   });
+*/
+
+MongoClient.connect(url, function(err, db) {
+  if (err) throw err;
+  var dbo = db.db("mydb");
+  var myobj = { name: "Company Inc", address: "Highway 37" };
+  dbo.collection("customers").insertOne(myobj, function(err, res) {
+    if (err) throw err;
+    console.log("1 document inserted");
+    db.close();
+  });
+});
+  
