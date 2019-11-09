6bb5d08091ad614215fc351e5c2256f105086c01
branabreo <github@brandondabreo.com>
2019-11-09 17:42:00 +0530

    Learning JS Update

--- FormatData.py
+++ FormatData.py
@@ -1,10 +1,11 @@
+import json
 import requests
 response = requests.get('https://www.quandl.com/api/v3/datasets/BSE/BOM533171.json?api_key=qz1RFMDVb5hEQ4qXxnUG')
-
-print response #print http status
-print(response.request.body) #print body which is empty
-print(response.request.url) #prints the url duh
-print(response.content) #prints all the content that is visible in the browser
-print(response.text) #de-serializizes json data
+##print(response)#print http status
+##print(response.request.body) #print body which is empty
+##print(response.request.url) #prints the url duh
+##print(response.content) #prints all the content that is visible in the browser
+##print(response.text) #de-serializizes json data
 rdata = json.loads(response.text) #puts raw json data into variable
-data_serialized = json.dumps(data)
+data_serialized = json.dumps(rdata) #dumps into json file
+
