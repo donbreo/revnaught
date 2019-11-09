6d3d30ccf604982194dc3ef6f4f978496b7c9e53
branabreo <github@brandondabreo.com>
2019-11-10 02:56:00 +0530

    Working Quandl API in JS

--- DbTest.js
+++ DbTest.js
@@ -1,7 +1,12 @@
 //create a database
 
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 5000;
+app.listen(PORT);
 var MongoClient = require('mongodb').MongoClient;
 var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
+const request = require('request');
 var url = "mongodb://localhost:27017/";
 /*
 MongoClient.connect(url, function(err, db) {
@@ -37,8 +42,28 @@ MongoClient.connect(url, function(err, db) {
 */
 
 //AJAX template
-var Http = new XMLHttpRequest();
-url='https://www.quandl.com/api/v3/datasets/BSE/BOM533171.json?api_key=qz1RFMDVb5hEQ4qXxnUG';
+/*
+const Http = new XMLHttpRequest();
+var url='https://www.quandl.com/api/v3/datasets/BSE/BOM533171.json?api_key=qz1RFMDVb5hEQ4qXxnUG';
 Http.open("GET", url);
 Http.send();
 console.log(Http.responseText);
+var obj = JSON.parse(Http.responseText);
+*/
+/*
+let request = new XMLHttpRequest();
+request.open('GET', 'https://www.quandl.com/api/v3/datasets/BSE/BOM533171.json?api_key=qz1RFMDVb5hEQ4qXxnUG', true);
+request.onload = function () {
+  // Convert JSON data to an object
+  let users = JSON.parse(this.response);
+request.send();
+*/
+
+request('https://www.quandl.com/api/v3/datasets/BSE/BOM533171.json',{json: true},(err, res, body) => {
+if (err) {return console.log(err)};
+console.log(body);
+if (res.statusCode == 200){
+//  console.log(body);
+}; 
+})
+
